fcc2601a8804bace550579607146cb1b59a6b80a
Nandhini <manivellanandhini@gmail.com>
2024-12-12 21:19:08 +0530

    java DOcker test

---
+++ ReverseNumber.java
@@ -0,0 +1,26 @@
+public class ReverseNumber {
+    public static void main(String[] args) {
+        System.out.print("Enter a number: ");
+        int num = System.in.read() - '0';
+        System.out.println();
+
+        int reversedNum = reverseNumber(num);
+
+        System.out.println("Reversed Number: " + reversedNum);
+    }
+
+    public static int reverseNumber(int num) {
+        int reversedNum = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reversedNum = reversedNum * 10 + digit;
+            num /= 10;
+        }
+
+        return reversedNum;
+    }
+}
+
+
+
